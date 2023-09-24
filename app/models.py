6bc9978e784ab38c7fcfcd06c3a6e744f5538074
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-24 11:36:10 +0100

    first config implementation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from app.extensions import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), unique=True)
