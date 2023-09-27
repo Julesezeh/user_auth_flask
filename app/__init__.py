dd7d4119751c0940769fd02e721441212103e1ac
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-27 10:31:42 +0100

    USING A CONFIG MODULE

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from app.extensions import api, db, migration
 from app.models import *
+from app.config import Config
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+    app.config.from_object(Config)
     api.init_app(app)
     db.init_app(app)
     migration.init_app(app, db)
