61cb2e57f5e43c792ab9f1e53f0bb449206ad554
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-28 17:58:11 +0100

    registering blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from app.extensions import api, db, migration
 from app.models import *
 from app.config import Config
+from app.blueprint import app_bp
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.register_blueprint(app_bp)
     api.init_app(app)
     db.init_app(app)
     migration.init_app(app, db)
