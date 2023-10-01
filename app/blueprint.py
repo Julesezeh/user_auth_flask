df3ee4d9ddedb2dcc90d667d0c8b0c2ab8a05e9f
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-01 10:26:04 +0100

    alternative approach

--- app/blueprint.py
+++ app/blueprint.py
@@ -1,9 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, Flask
+import logging
 
+app = Flask(__name__)
+# logger = logging.getLogger(__name__)
 
 app_bp = Blueprint("app_blueprint", __name__)
 
 
 @app_bp.route("/")
 def index():
+    app.logger.info("Works fine")
     return "Hello World"
