0afcb5bfaa19902eabf76e5e3418a6311cdc4327
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-01 10:26:23 +0100

    can't access blueprint in resources file

--- app/resources.py
+++ app/resources.py
@@ -1,7 +1,9 @@
 from .blueprint import app_bp
+from app import logger
 from flask import request, Response, render_template
 
 
 @app_bp.register("/")
 def index():
+    logger.info("works fine")
     return "Hello World"
