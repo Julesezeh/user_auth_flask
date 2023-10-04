daa8881685188c4ae9f211bc9cfd39dbda8253bc
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-10-04 10:29:13 +0100

    Logs still not getyting displayed

--- app/resources.py
+++ app/resources.py
@@ -5,5 +5,5 @@ from flask import current_app
 
 @app.route("/")
 def index():
-    current_app.logger.info("Keep the streak alive")
+    current_app.logger.info("Log still not getting displayed")
     return "Hello World"
